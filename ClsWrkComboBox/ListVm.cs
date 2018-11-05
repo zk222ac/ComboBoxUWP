2c023ac66e95bed3aa143dc4c9a812d87c56bf8a
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-11-05 14:50:31 +0100

    Add project files.

---
+++ ClsWrkComboBox/ListVm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsWrkComboBox
+{
+  public class ListVm
+  {
+      private List<string> _list;
+        public ListVm()
+        {
+            _list = new List<string>() {"Mr", "Mrs", "Ms", "Miss"};
+        }
+
+      public List<string> List
+      {
+          get { return this._list; }
+          set { this._list = value; }
+      }
+
+    }
+}
+
